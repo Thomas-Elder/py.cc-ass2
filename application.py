0b6e2e6615c9b275edcce1fc67acfb97f6595b70
Thomas <thombsaway@gmail.com>
2021-07-13 20:58:37 +1000

    Rename main

--- application.py
+++ application.py
@@ -1,3 +1,8 @@
+from waitress import serve
+
+import logging
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
 
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -11,4 +16,5 @@ def index():
 # For local hosting
 #
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8181, debug=True)
+    #app.run(host='127.0.0.1', port=8181, debug=True)
+    serve(app, host='0.0.0.0', port=80)
