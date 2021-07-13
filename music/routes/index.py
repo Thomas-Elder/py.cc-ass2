49805d6979f9eeac5b0833b344c3a29b299a9a61
Thomas <thombsaway@gmail.com>
2021-07-13 21:52:57 +1000

    set up basic index route.

---
+++ music/routes/index.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+
+bp = Blueprint('index', __name__, url_prefix='/index')
+
+@bp.route('/')
+@bp.route('/index', methods=("GET", "POST"))
+def index():
+
+    return render_template('index.html')
