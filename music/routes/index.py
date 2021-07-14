dbf8c37dfa153252951850a41eb82932bdac7d2e
Thomas <thombsaway@gmail.com>
2021-07-14 11:23:56 +1000

    Update methods handled by route

--- music/routes/index.py
+++ music/routes/index.py
@@ -3,7 +3,7 @@ from flask import Blueprint, flash, g, redirect, render_template, request, sessi
 bp = Blueprint('index', __name__, url_prefix='/index')
 
 @bp.route('/')
-@bp.route('/index', methods=("GET", "POST"))
+@bp.route('/index', methods=("GET"))
 def index():
 
     return render_template('index.html')
