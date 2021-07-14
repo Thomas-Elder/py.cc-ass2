68f123c893fe7b1a6cc3a5f5b89cd05fcbb7565c
Thomas <thombsaway@gmail.com>
2021-07-14 21:30:52 +1000

    testing db, adding record and retrieving, then passing to view

--- music/routes/index.py
+++ music/routes/index.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
+from music.db_spike import get_movie, put_movie
 
 bp = Blueprint('index', __name__, url_prefix='/index')
 
 @bp.route('/')
-@bp.route('/index', methods=("GET"))
+@bp.route('/index', methods=("GET", "POST"))
 def index():
 
-    return render_template('index.html')
+    put_movie("The Big New Movie", 2015, "Nothing happens at all.", 0)
+
+    data = get_movie("The Big New Movie", 2015)
+
+    return render_template('index.html', data=data)
