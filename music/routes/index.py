2f8c18b28f7ee2ad2163223343bbad25504085af
Thomas <thombsaway@gmail.com>
2021-07-22 20:32:58 +1000

    Testing get functions for users and songs.

--- music/routes/index.py
+++ music/routes/index.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
-from music.db.database import get_user, put_user
+from music.db.database import get_user, put_user, get_users, get_song, get_songs
 
 bp = Blueprint('', __name__)
 
 @bp.route('/')
 def index():
 
-    data = get_user('s33750870@student.rmit.edu.au')
+    user = get_user(email='s33750870@student.rmit.edu.au')
+    all_users = get_users()
 
-    return render_template('index.html', data=data)
+    song = get_song(title='1904', artist='The Tallest Man on Earth')
+    all_songs = get_songs()
+
+    return render_template('index.html', user=user, all_users=all_users, song=song, all_songs=all_songs)
