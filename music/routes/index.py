6a6810db88c68a2af4022a5b311757d652c6abc3
Thomas <thombsaway@gmail.com>
2021-07-22 15:43:00 +1000

    Getting data from new db module.

--- music/routes/index.py
+++ music/routes/index.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, g
-from music.db_spike import get_movie, put_movie
+from music.database import get_user, put_user
 
 bp = Blueprint('', __name__)
 
 @bp.route('/')
 def index():
 
-    put_movie("The Big New Movie", 2015, "Nothing happens at all.", 0)
-
-    data = get_movie("The Big New Movie", 2015)
+    data = get_user('s33750870@student.rmit.edu.au', 'Tom Elder0')
 
     return render_template('index.html', data=data)
