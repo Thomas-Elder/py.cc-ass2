99c52ecf6a0d1d2dbb54e340499231ade90751b1
Thomas <thombsaway@gmail.com>
2021-07-26 14:07:16 +1000

    Simplify app set up.

--- music/__init__.py
+++ music/__init__.py
@@ -4,14 +4,7 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev'
-    )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+    app.config.from_mapping(SECRET_KEY='dev')
 
     from .routes import authentication, subscription, index
     app.register_blueprint(authentication.bp)
