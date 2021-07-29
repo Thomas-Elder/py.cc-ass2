d781268c22e3da945824fe6931c423ee02610e9e
Thomas <thombsaway@gmail.com>
2021-07-29 16:22:13 +1000

    Add login_manager

--- music/__init__.py
+++ music/__init__.py
@@ -1,10 +1,17 @@
 import os
 
 from flask import Flask
+from flask_login import login_manager, login_user, logout_user, login_required, current_user, LoginManager
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev')
+
+    loginManager = LoginManager(app)
+    loginManager.login_view = 'authentication/login'
+
+    
 
     from .routes import authentication, subscription, index
     app.register_blueprint(authentication.bp)
@@ -14,5 +21,9 @@ def create_app(test_config=None):
     from .db import dynamodb, s3
     dynamodb.init_app(app)
     s3.init_app(app)
+
+    @loginManager.user_loader
+    def load_user(id):
+        return dynamodb.get_user(id)
 
     return app
