b04be365e3d4e2f2b91acd6cc34b8edb0197ab12
Thomas <thombsaway@gmail.com>
2021-07-29 16:59:33 +1000

    Add get_img call to test it works.

--- music/routes/subscription.py
+++ music/routes/subscription.py
@@ -1,9 +1,10 @@
 
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from ..db import s3
 
 bp = Blueprint('subscription', __name__, url_prefix='/subscription')
 
 @bp.route('/music', methods=("GET", "POST"))
 def music():
-
+    s3.get_img()
     return render_template('subscription/music.html')
