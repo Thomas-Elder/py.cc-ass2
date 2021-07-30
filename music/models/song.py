e41ba0e15503e34785ac7d2f49dac90f13ed9a6c
Thomas <thombsaway@gmail.com>
2021-07-30 12:05:01 +1000

    Set up Song class

---
+++ music/models/song.py
@@ -0,0 +1,7 @@
+class Song:
+    def __init__(self, artist, title, year, web_url, img_url=None):
+        self.artist = artist
+        self.title = title
+        self.year = year
+        self.web_url = web_url
+        self.img_url = img_url
