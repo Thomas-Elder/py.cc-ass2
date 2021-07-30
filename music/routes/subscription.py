be0c28078586e5ff4e214cf387e79d09648911aa
Thomas <thombsaway@gmail.com>
2021-07-30 14:52:24 +1000

    Getting and passing song data to view.

--- music/routes/subscription.py
+++ music/routes/subscription.py
@@ -2,11 +2,17 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from flask_login import current_user
 
-from ..db import s3
+from ..db.s3 import get_img
+from ..db.dynamodb import get_songs
 
 bp = Blueprint('subscription', __name__, url_prefix='/subscription')
 
 @bp.route('/music', methods=("GET", "POST"))
 def music():
+    
+    songs = get_songs() #list
 
-    return render_template('subscription/music.html', current_user=current_user)
+    for song in songs:
+        song.img_url = get_img(song.artist)
+
+    return render_template('subscription/music.html', current_user=current_user, songs=songs)
