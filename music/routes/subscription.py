c2a6fc524090407bcbef693a24c84294f867af58
Thomas <thombsaway@gmail.com>
2021-07-30 15:24:30 +1000

    Added dev setting so we can dev without accessing s3

--- music/routes/subscription.py
+++ music/routes/subscription.py
@@ -1,18 +1,26 @@
+import os
 
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from flask_login import current_user
 
 from ..db.s3 import get_img
 from ..db.dynamodb import get_songs
+from .forms import QueryForm
 
 bp = Blueprint('subscription', __name__, url_prefix='/subscription')
 
 @bp.route('/music', methods=("GET", "POST"))
 def music():
     
+    form = QueryForm()
+
     songs = get_songs() #list
 
-    for song in songs:
-        song.img_url = get_img(song.artist)
+    if os.environ['FLASK_ENV'] == "dev":
+        for song in songs:
+            song.img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png"
+    else:
+        for song in songs:
+            song.img_url = get_img(song.artist)
 
-    return render_template('subscription/music.html', current_user=current_user, songs=songs)
+    return render_template('subscription/music.html', current_user=current_user, songs=songs, form=form)
