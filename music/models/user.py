cb06d859e34aefaa687ae6f674e3d59eef899427
Thomas <thombsaway@gmail.com>
2021-07-31 13:33:51 +1000

    Add usersongs to User.

--- music/models/user.py
+++ music/models/user.py
@@ -2,8 +2,9 @@ from flask_login import UserMixin
 
 class User(UserMixin):
 
-    def __init__(self, id, email, username, password):
+    def __init__(self, id, email, username, password, usersongs):
         self.id = id
         self.email = email
         self.username = username
         self.password = password
+        self.usersongs = usersongs
